0d12324837945a405baac761afb1ca11e79462b5
Luis Felipe Hernandez <17690241+lf-hernandez@users.noreply.github.com>
2023-10-24 20:07:15 +0100

    Remove unnecessary test

--- test_main.py
+++ test_main.py
@@ -1,6 +1,5 @@
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
-import pytest
 from fastapi.testclient import TestClient
 
 from main import app
@@ -32,17 +31,3 @@ def test_websocket_endpoint(mock_openai):
         websocket.send_text("Hello")
         data = websocket.receive_text()
         assert "Mocked response" in data
-
-
-def test_data_processing():
-    event_sample = {"choices": [{"delta": {"content": "Sample response."}}]}
-
-    chunk_message = ""
-    if (
-        "choices" in event_sample
-        and event_sample["choices"][0].get("delta")
-        and "content" in event_sample["choices"][0]["delta"]
-    ):
-        chunk_message = event_sample["choices"][0]["delta"]["content"]
-
-    assert chunk_message == "Sample response."
